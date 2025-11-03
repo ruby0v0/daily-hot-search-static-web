814929ab2d3e1bdb7c5bc3a3104655f2b8c7d6f2
鲨鱼辣椒 <monishine_code@163.com>
2025-11-03 20:34:43 +0800

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,8 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
